2b94be05b60448f3f1b851359c3f0187e6a1739c
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-09-12 00:25:26 -0400

    fix homeroutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,9 +5,6 @@ const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, (req, res) => {
     Post.findAll({
-        where: {
-            user_id: req.session.user_id
-        },
         attributes: ['id', 'title', 'content', 'created_at'],
 
         include: [{
@@ -23,7 +20,7 @@ router.get('/', withAuth, (req, res) => {
     })
         .then(dbPostData => {
             const posts = dbPostData.map(post => post.get({ plain: true }));
-            res.render('dashboard', { posts, loggedIn: true });
+            res.render('homepage', { posts, loggedIn: true });
         })
         .catch(err => {
             console.log(err);
@@ -31,7 +28,18 @@ router.get('/', withAuth, (req, res) => {
         });
 });
 
-router.get('/edit/:id', withAuth, (req, res) => {
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+router.get('/signup', (reqq, res) => {
+    res.render('signup');
+});
+
+router.get('/post/:id', withAuth, (req, res) => {
     Post.findOne({
         where: {
             id: req.params.id
@@ -58,16 +66,46 @@ router.get('/edit/:id', withAuth, (req, res) => {
                 return;
             }
             const post = dbPostData.get({ plain: true });
-            res.render('edit-post', { post, loggedIn: true });
+            res.render('single-post', { post, loggedIn: true });
         })
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
         });
-})
-
-router.get('/new', (req, res) => {
-    res.render('new-post');
 });
 
+router.get('/post-comment', withAuth, (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'title', 'content', 'created_at'],
+        
+        include: [{
+            model: User,
+            attributes: ['username']
+        },
+        {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post with this ID' });
+            return;
+        }
+        const post = dbPostData.get({ plain: true });
+        res.render('post-comment', { post, loggedIn: true });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 module.exports = router;
