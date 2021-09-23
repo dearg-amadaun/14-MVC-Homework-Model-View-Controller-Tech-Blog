4da775ad0687bde90c05b441e1e010b2d6e7714c
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-09-23 16:56:05 -0400

    homepage works

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,7 +8,7 @@ router.get('/', withAuth, (req, res) => {
         order: [[ 'created_at', 'DESC']],
         include: [{
             model: Comment,
-            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            attributes: ['id', 'post_id', 'user_id', 'created_at'],
             include: {
                 model: User,
                 attributes: ['username']
