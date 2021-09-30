0f1cf6eadae4b07503e1f65d8c0f8e69762c065d
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-09-30 15:04:24 -0400

    Dashboard styling, create post button working.

--- public/js/addPost.js
+++ public/js/addPost.js
@@ -1,26 +1,26 @@
 async function newFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const title = document.querySelector(`input[name="post-title"]`).value;
-    const content = document.querySelector(`input[name="post-content"]`).value;
-      
-      const response = await fetch('/api/posts', {
-        method: 'POST',
-        body: JSON.stringify({
-          title,
-          content
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-      
-      if (response.ok) {
-        document.location.replace('/dashboard');
-      } else {
-        alert(response.statusText);
+  const title = document.querySelector(`input[name="post-title"]`).value;
+  const content = document.querySelector(`input[name="post-content"]`).value;
+    
+    const response = await fetch('/api/posts', {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        content
+      }),
+      headers: {
+        'Content-Type': 'application/json'
       }
+    });
+    
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
 
 }
 
-document.querySelector('.new-post').addEventListener('submit', newFormHandler);
+document.querySelector('.post-create').addEventListener('submit', newFormHandler);
